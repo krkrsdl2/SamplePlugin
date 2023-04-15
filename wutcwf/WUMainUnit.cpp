4a582023a17d9174d3e37d350fdc39c3683bc251
uyjulian <uyjulian@gmail.com>
2023-04-14 23:46:19 -0500

    remove istream_compat

--- wutcwf/WUMainUnit.cpp
+++ wutcwf/WUMainUnit.cpp
@@ -17,7 +17,6 @@ int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason, void* lpReserved
 #endif
 #include "WaveIntf.h"
 #ifndef _WIN32
-#include "istream_compat.h"
 #define byte BYTE
 #define __int64 int64_t
 #endif
